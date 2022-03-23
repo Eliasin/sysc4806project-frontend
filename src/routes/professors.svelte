c6e50e9c7246ec328621568e0e8340c648278967
Callum Coughlan <callum.d.w.coughlan@gmail.com>
2022-03-23 04:15:27 -0400

    Add edit to professor

---
+++ src/routes/professors.svelte
@@ -0,0 +1,29 @@
+<svelte:head>
+    <title>SYSC4806 Project - Professors</title>
+</svelte:head>
+
+<script>
+    function selectCreate() {
+        window.location.pathname = "/professors/new/";
+    }
+    function selectView() {
+        window.location.pathname = "/professors/list/";
+    }
+</script>
+
+<body>
+    <div class="selection">
+        <div id="selection-a">
+            <button class="select" on:click={selectCreate}>
+                Create Professor
+            </button>
+        </div>
+        <div id="selection-b">
+            <h2 class="selection-header">
+            </h2>
+            <button class="select" on:click={selectView}>
+                List Professors
+            </button>
+        </div>
+    </div>
+</body>
--- src/routes/professors/list.svelte
+++ src/routes/professors/list.svelte
@@ -3,37 +3,58 @@
 </svelte:head>
 
 <script>
-    import { APP_URL } from '../../globals';
+    import { HEROKU_URL } from '../../globals';
+    import { browser } from '$app/env';
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
 
     async function fetchProfessors() {
-        const response = await fetch(APP_URL + '/rest/professors',
+        const response = await fetch(HEROKU_URL + '/rest/professor',
         {
             method: 'GET',
         });
         return response.json();
     }
 
-    fetchProfessors().then(data => {
-        const list = document.getElementById('professor-list');
+    onMount(() => {
+        fetchProfessors().then(data => {
+            const list = document.getElementById('professor-list');
 
-        data.forEach(professor => {
-            const li = document.createElement('li');
-            li.class = 'professor-list-item';
-            
-            const id = document.createElement('span');
-            id.innerText = professor.id;
-            li.appendChild(id);
-            
-            const name = document.createElement('span');
-            name.innerText = professor.name;
-            li.appendChild(name);
-            
-            list.appendChild(li);
+            data.forEach(professor => {
+                const li = document.createElement('li');
+                li.class = 'professor-list-item';
+                
+                const id = document.createElement('span');
+                id.innerText = professor.id;
+                li.appendChild(id);
+                
+                const name = document.createElement('span');
+                name.innerText = professor.name;
+                li.appendChild(name);
+
+                const editButton = createElement('button');
+                editButton.onclick = goto(VERCEL_URL + '/professors/edit?id=' + id);
+                li.appendChild(editButton);
+                
+                list.appendChild(li);
+            });
         });
-    });
+    })
+
+    /*
+    async function fetchEdit() {
+        async () => {
+            const response = await fetch(APP_URL + '/professors/edit?id=',
+            {
+                method: 'POST',
+            });
+            return response.json();
+        }
+    }
+    */
 
     function doBack() {
-        goto(VERCEL_URL + '/professors/');
+        window.location.pathname = "/professors/";
     }
 
 </script>
@@ -42,8 +63,5 @@
     <div class="professor-list-container">
         <button id="back-button" on:click={doBack}>Back</button>
         <ul id="professor-list" />
-        <button id="edit-button" on:click={() => {
-                 window.location.href = window.location.origin + '/professor/';
-        }}>Edit</button>
     </div>
 </body>
