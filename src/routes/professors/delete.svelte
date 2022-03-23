751ee374422a495ff3fdf954f315c62a58ae3e28
Kaue Oliveira <elmovckaue@gmail.com>
2022-03-22 23:15:04 -0400

    Update packages and add favicon

---
+++ src/routes/professors/delete.svelte
@@ -0,0 +1,32 @@
+<script>
+    import { APP_URL } from '../../globals';
+
+    async function deleteProfessor () {
+        const professorJSON = JSON.stringify({
+            name: document.getElementById('name').value,
+        })
+        
+        if (professorJSON.includes('""')) {
+            return;
+        }
+        
+        await fetch(APP_URL + '/rest/appprofesor', {
+            method: 'POST',
+            body: professorJSON,
+        });
+    
+        window.location.href = APP_URL + '/professors/';
+    }
+</script>
+
+<body>
+    <div class="applicant-form-container">
+        <form id="applicant-form">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" placeholder="Enter name">
+            </div>
+            <button type="submit" class="btn btn-primary" onclick="createApplicant()">Submit</button>
+        </form>
+    </div>
+</body>
---
+++ src/routes/professors/new.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { HEROKU_URL, VERCEL_URL } from '../../globals';
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+
+    async function createProfessor () {
+        const professorJSON = JSON.stringify({
+            name: document.getElementById('name').value,
+        })
+        
+        if (professorJSON.includes('""')) {
+            return;
+        }
+        
+        await fetch(APP_URL + '/rest/professor', {
+            method: 'POST',
+            body: professorJSON,
+        });
+    
+        window.location.href = APP_URL + '/professor/';
+    }
+</script>
+
+<body>
+    <div class="professor-form-container">
+        <form id="professor-form">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" placeholder="Enter name">
+            </div>
+            <button type="submit" class="btn btn-primary" onclick="createProfessor()">Submit</button>
+        </form>
+    </div>
+</body>
