649370631b57f3c2039dff6ad952614af7464085
Kaue oliveira <elmovckaue@gmail.com>
2022-04-06 13:38:24 -0400

    Fix edit page nesting

--- src/routes/professors/_edit.svelte
+++ src/routes/professors/_edit.svelte
@@ -64,93 +64,93 @@ import Applicants from '../applicants.svelte';
                 <label for="name">Name</label>
                 <input type="text" class="form-control" id="name" placeholder="Enter name">
             </div>
-            <div id="researched-list" class="labeled-list">
-                <label for="researched-field-list">Researched Fields</label>
-                <ul id="researched-field-list">
-                    {#each professorResearchFields as researchField}
-                    <li>
-                        <span>{researchField.id}</span>
-                        <option value={researchField.id}>{researchField.name}</option>
-                        <button on:click={async (e) => {
-                            e.preventDefault();
-                            await removeProfessorResearchField(sessionToken, professorId, researchField.id);
-
-                            professorResearchFields = await fetchProfessorResearchFields(sessionToken, professorId);
-                        }}>Delete</button>
-                    </li>
-                    {/each}
-                </ul>
-            </div>
-            <div id="research-list" class="labeled-list">
-                <label for="field-list">Other Fields</label>
-                <ul id="field-list">
-                    {#each fieldsNotResearched as researchField}
-                    <li>
-                        <span>{researchField.id}</span>
-                        <option value={researchField.id}>{researchField.name}</option>
-                        <button on:click={async (e) => {
-                            e.preventDefault();
-                            await addResearchedFieldToProfessor(sessionToken, professorId, researchField.id);
-
-                            professorResearchFields = await fetchProfessorResearchFields(sessionToken, professorId);
-                        }}>Add</button>
-                    </li>
-                    {/each}
-                </ul>
-            </div>
-            <div id="pending-list" class="labeled-list">
-                <label for="pending-application">Pending Applications</label>
-                <ul id="pending-applications">
-                    {#each pendingApplicants as applicant}
-                    <li type="button" class="collapsible">
-                        <div>Name: {applicant.name}</div>
-                        <div>Email: {applicant.email}</div>
-                        <div>Field: {applicant.desired_field}</div>
-                        <button on:click={async (e) => {
-                            e.preventDefault();
-                            await acceptApplication(sessionToken, professorId, applicant.id);
-
-                            pendingApplicants = await fetchApplicantsForProfessor(sessionToken, professorId, 'PENDING');
-                            approvedApplicants = await fetchApplicantsForProfessor(sessionToken, professorId, 'ACCEPTED');
-                        }}>Approve</button>
-                        <button on:click={async (e) => {
-                            e.preventDefault();
-                            await denyApplication(sessionToken, professorId, applicant.id);
-
-                            pendingApplicants = await fetchApplicantsForProfessor(sessionToken, professorId, 'PENDING');
-                            rejectedApplicants = await fetchApplicantsForProfessor(sessionToken, professorId, 'DENIED');
-                        }}>Reject</button>
-                    </li>
-                    <div class="content">
-                        <ApplicantFiles applicantId={applicant.id.toString()}/>
-                    </div>
-                    {/each}
-                </ul>
-            </div>
-            <div id="approved-list" class="labeled-list">
-                <label for="approved-application">Approved Applications</label>
-                <ul id="approved-applications">
-                    {#each approvedApplicants as applicant}
-                    <li type="button" class="collapsible">{applicant.name + ' ' + applicant.email}</li>
-                    <div class="content">
-                        <ApplicantFiles applicantId={applicant.id.toString()}/>
-                    </div>
-                    {/each}
-                </ul>
-            </div>
-            <div id="rejected-list" class="labeled-list">
-                <label for="rejected-application">Rejected Applications</label>
-                <ul id="rejected-applications">
-                    {#each rejectedApplicants as applicant}
-                    <li type="button" class="collapsible">{applicant.name + ' ' + applicant.email}</li>
-                    <div class="content">
-                        <ApplicantFiles applicantId={applicant.id.toString()}/>
-                    </div>
-                    {/each}
-                </ul>
-            </div>
-            <button class="btn btn-primary" on:click={requestEditProfessorName}>Submit</button>
         </form>
+        <div id="researched-list" class="labeled-list">
+            <label for="researched-field-list">Researched Fields</label>
+            <ul id="researched-field-list">
+                {#each professorResearchFields as researchField}
+                <li>
+                    <span>{researchField.id}</span>
+                    <option value={researchField.id}>{researchField.name}</option>
+                    <button on:click={async (e) => {
+                        e.preventDefault();
+                        await removeProfessorResearchField(sessionToken, professorId, researchField.id);
+
+                        professorResearchFields = await fetchProfessorResearchFields(sessionToken, professorId);
+                    }}>Delete</button>
+                </li>
+                {/each}
+            </ul>
+        </div>
+        <div id="research-list" class="labeled-list">
+            <label for="field-list">Other Fields</label>
+            <ul id="field-list">
+                {#each fieldsNotResearched as researchField}
+                <li>
+                    <span>{researchField.id}</span>
+                    <option value={researchField.id}>{researchField.name}</option>
+                    <button on:click={async (e) => {
+                        e.preventDefault();
+                        await addResearchedFieldToProfessor(sessionToken, professorId, researchField.id);
+
+                        professorResearchFields = await fetchProfessorResearchFields(sessionToken, professorId);
+                    }}>Add</button>
+                </li>
+                {/each}
+            </ul>
+        </div>
+        <div id="pending-list" class="labeled-list">
+            <label for="pending-application">Pending Applications</label>
+            <ul id="pending-applications">
+                {#each pendingApplicants as applicant}
+                <li type="button" class="collapsible">
+                    <div>Name: {applicant.name}</div>
+                    <div>Email: {applicant.email}</div>
+                    <div>Field: {applicant.desired_field}</div>
+                    <button on:click={async (e) => {
+                        e.preventDefault();
+                        await acceptApplication(sessionToken, professorId, applicant.id);
+
+                        pendingApplicants = await fetchApplicantsForProfessor(sessionToken, professorId, 'PENDING');
+                        approvedApplicants = await fetchApplicantsForProfessor(sessionToken, professorId, 'ACCEPTED');
+                    }}>Approve</button>
+                    <button on:click={async (e) => {
+                        e.preventDefault();
+                        await denyApplication(sessionToken, professorId, applicant.id);
+
+                        pendingApplicants = await fetchApplicantsForProfessor(sessionToken, professorId, 'PENDING');
+                        rejectedApplicants = await fetchApplicantsForProfessor(sessionToken, professorId, 'DENIED');
+                    }}>Reject</button>
+                </li>
+                <div class="content">
+                    <ApplicantFiles applicantId={applicant.id.toString()}/>
+                </div>
+                {/each}
+            </ul>
+        </div>
+        <div id="approved-list" class="labeled-list">
+            <label for="approved-application">Approved Applications</label>
+            <ul id="approved-applications">
+                {#each approvedApplicants as applicant}
+                <li type="button" class="collapsible">{applicant.name + ' ' + applicant.email}</li>
+                <div class="content">
+                    <ApplicantFiles applicantId={applicant.id.toString()}/>
+                </div>
+                {/each}
+            </ul>
+        </div>
+        <div id="rejected-list" class="labeled-list">
+            <label for="rejected-application">Rejected Applications</label>
+            <ul id="rejected-applications">
+                {#each rejectedApplicants as applicant}
+                <li type="button" class="collapsible">{applicant.name + ' ' + applicant.email}</li>
+                <div class="content">
+                    <ApplicantFiles applicantId={applicant.id.toString()}/>
+                </div>
+                {/each}
+            </ul>
+        </div>
+        <button class="btn btn-primary" on:click={requestEditProfessorName}>Submit</button>
     </div>
 
     {#if noBack}
