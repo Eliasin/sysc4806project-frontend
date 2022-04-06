de6ee64add6b5cb9d1195f2d1893b29b5142a063
Steven Pham <steven.pham.8.2000@gmail.com>
2022-04-06 09:29:38 -0400

    Fix heroku url kept as localhost

--- src/globals.js
+++ src/globals.js
@@ -1,2 +1,1 @@
-//export const HEROKU_URL = 'https://sysc4806-rust.herokuapp.com';
-export const HEROKU_URL = 'http://localhost:8000';
+export const HEROKU_URL = 'https://sysc4806-rust.herokuapp.com';
